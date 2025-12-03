60e35a8c898f34ee869cc08f54675ad49149da5b
EduRoDev <solano934@gmail.com>
2025-12-03 13:03:46 -0500

    fix: arreglo en el app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { DatabaseConfigService } from './database-config/database-config.service';
-import { DatabaseService } from './database/database.service';
-import { DatabaseModule } from './database/database.module';
+
 
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-    }),
-    DatabaseModule
+    })
   ],
   controllers: [],
-  providers: [DatabaseConfigService, DatabaseService],
+  providers: [],
 })
 export class AppModule {}
