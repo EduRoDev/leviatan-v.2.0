db8af6adf1476a275e082485add2b4babf3dd3d4
EduRoDev <solano934@gmail.com>
2025-12-03 13:00:58 -0500

    feat: configuracion para conexion a la db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseConfigService } from './database-config/database-config.service';
+import { DatabaseService } from './database/database.service';
+import { DatabaseModule } from './database/database.module';
+
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DatabaseModule
+  ],
   controllers: [],
-  providers: [],
+  providers: [DatabaseConfigService, DatabaseService],
 })
 export class AppModule {}
